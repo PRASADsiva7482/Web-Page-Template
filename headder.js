8a03597b4d7dba6520c6926c77b0556cce688da7
PRASADsiva7482 <58835774+PRASADsiva7482@users.noreply.github.com>
2020-12-13 10:48:16 +0530

    Add files via upload

---
+++ headder.js
@@ -0,0 +1,16 @@
+function header(){
+    var head="Title Data";
+    document.getElementById("header").innerHTML=head; 
+    
+    var foot="Footer Data";
+    document.getElementById("footer").innerHTML=foot;
+    
+    var link="Left Data";
+    document.getElementById("left").innerHTML=link;
+
+    var elink="Right Data";
+    document.getElementById("right").innerHTML=elink;
+
+    var navbar="Nav Bar Data";
+    document.getElementById("navbar").innerHTML=navbar;
+}
